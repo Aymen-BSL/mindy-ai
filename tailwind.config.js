a12b0799a32d85edeb060f850d55e86eb9c971a5
Mohamed chehata <60447559+Nero-eng@users.noreply.github.com>
2025-02-08 01:25:07 +0100

    i finished the page 1 --v 1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Include all files in the `app` directory
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // Include components if you have any
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
